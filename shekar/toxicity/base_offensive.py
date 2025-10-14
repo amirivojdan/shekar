4cd6e4299c3e61b34d48bba5716616f4bedf91e0
Ahmad Amirivojdan <amirivojdan@gmail.com>
2025-10-14 02:51:00 -0400

    Add offensive language detection
    Introduces a new toxicity module with an offensive language classifier using ONNX.

---
+++ shekar/toxicity/base_offensive.py
@@ -0,0 +1,19 @@
+from shekar.base import BaseTransform
+from .logistic_offensive_classifier import LogisticOffensiveClassifier
+
+POS_REGISTRY = {
+    "logistic_offensive": LogisticOffensiveClassifier,
+}
+
+class OffensiveLanguageClassifier(BaseTransform):
+    def __init__(self, model: str = "logistic_offensive", model_path=None):
+        model = model.lower()
+        if model not in POS_REGISTRY:
+            raise ValueError(
+                f"Unknown model '{model}'. Available: {list(POS_REGISTRY.keys())}"
+            )
+
+        self.model = POS_REGISTRY[model](model_path=model_path)
+
+    def transform(self, X: str) -> list:
+        return self.model.transform(X)
---
+++ shekar/toxicity/logistic_offensive_classifier.py
@@ -0,0 +1,34 @@
+from shekar.base import BaseTransform
+from shekar.hub import Hub
+from pathlib import Path
+import onnxruntime
+import numpy as np
+from shekar.utils import get_onnx_providers
+from shekar.preprocessing import StopWordRemover
+
+class LogisticOffensiveClassifier(BaseTransform):
+    def __init__(self, model_path: str | Path = None):
+        super().__init__()
+        resource_name = "tfidf_logistic_offensive.onnx"
+        if model_path is None or not Path(model_path).exists():
+            model_path = Hub.get_resource(file_name=resource_name)
+
+        self.session = onnxruntime.InferenceSession(
+            model_path, providers=get_onnx_providers()
+        )
+
+        self.id2label = {0: "neutral", 1: "offensive"}
+        self.stopword_remover = StopWordRemover()
+    
+    def transform(self, X: str) -> str:
+        X = self.stopword_remover(X)
+
+        in_name = self.session.get_inputs()[0].name
+        out_names = [o.name for o in self.session.get_outputs()]
+        arr = np.array([[X]], dtype=object)
+        onnx_label, onnx_proba = self.session.run(out_names, {in_name: arr})
+
+        if onnx_proba.ndim != 2:
+            onnx_label, onnx_proba = onnx_proba, onnx_label
+
+        return (self.id2label[onnx_label[0]], float(onnx_proba[0][onnx_label[0]]))
